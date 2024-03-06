ecd73cad15f04caeb971930745f5a6d61caebc46
Sivaganesh Natarajavel <sivaganeshnatarajavel@gmail.com>
2024-03-06 09:33:01 +0530

    e

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+app.get("/", async (req, res) => {
+  res.send("Vanakkam makkaley");
+});
+
+app.get("/json", async (req, res) => {
+  res.send({ message: "pun" });
+});
